808a59026d93a13e8ddef52b1c4cefc34bc03e37
dinesh <10125582+Dinesh2521@users.noreply.github.com>
2018-12-15 04:07:41 +0530

    For implemnting basi heartbeat into any grpc service

---
+++ client/HeartbeatClient.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+
+	pb "grpcHeratbeat/api"
+)
+
+const (
+	address = "localhost:25000"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Unable to connect to grpc endpoint: %v", err)
+	}
+	defer conn.Close()
+
+	// create the client instance
+	client := pb.NewHeartbeatClient(conn)
+
+	// connect to the server and call the required method
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	response, err := client.GetHeartbeat(ctx, &pb.Empty{})
+	//c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("Error in calling the heartbeat service : %v", err)
+	}
+	log.Printf("Service heartbeat received : %s", response.Message)
+}
---
+++ server/HeartbeatServer.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+
+	pb "grpcHeratbeat/api"
+)
+
+//go:generate protoc -I ../api/ ../api/heartbeat.proto --go_out=plugins=grpc:../api
+
+const (
+	port = ":25000"
+)
+
+// server is used to implement api.HeartbeatServer
+type server struct{}
+
+// SayHello implements api.HeartbeatServer
+func (s *server) GetHeartbeat(ctx context.Context, in *pb.Empty) (*pb.HeartbeatMsg, error) {
+	//TODO add any service specific code determine the tatus and return the results
+	return &pb.HeartbeatMsg{ServiceName: "sample1", Timestamp: 1544823909, IsRunning: true, Message: "500 OK"}, nil
+}
+
+// Run the gRPC server
+func main() {
+	// open the port and start listening to it
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen to port : %v", err)
+	}
+	// create a new grpc server instance
+	grpcServer := grpc.NewServer()
+
+	//register the heartbeat server
+	pb.RegisterHeartbeatServer(grpcServer, &server{})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(grpcServer)
+
+	// bing the port through which clients cn connect
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to expose the end point: %v", err)
+	}
+}
